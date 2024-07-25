f74ffeaf22dc6af998a09054bde6fe806d7234a5
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-25 19:23:17 +0200

    instance my object and call its methods to the app file, finished!

--- java-list/src/main/java/dev/arturo/App.java
+++ java-list/src/main/java/dev/arturo/App.java
@@ -10,7 +10,19 @@ public static void main(String[] args) {
 
     Week week = new Week();
 
-    System.out.println(week.clearAll());
+    System.out.println("The days of the week are" + week.getDaysOfTheWeek().toString());
+
+    System.out.println("The size of our list is of " + week.longList());
+
+    System.out.println("i hate the mondays soooo...do i make bip with monday?" + week.removeArgument("monday"));
+
+    System.out.println(week.getSolicitedDay("friday"));
+
+    System.out.println(week.itExists("Agumon"));
+
+    System.out.println("i am gonna show you the days in alph order" + week.alphabeticOrder());
+
+    System.out.println("sooo...i dont want more days" + week.clearAll());
 }
 }
     
